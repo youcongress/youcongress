ffd29b52a127bad212cdc3189f9179d1241ee230
Hector Perez <hecpeare@gmail.com>
2023-12-17 16:20:20 +0100

    Home for non-logged visitors

--- test/you_congress_web/controllers/page_controller_test.exs
+++ test/you_congress_web/controllers/page_controller_test.exs
@@ -3,7 +3,9 @@ defmodule YouCongressWeb.PageControllerTest do
 
   test "GET / loads successfully", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Welcome to YouCongress"
+
+    assert html_response(conn, 200) =~
+             "The biggest problem in the world is not climate change, war or poverty, but how we organise among ourselves to make good decisions and carry them out"
   end
 
   test "GET /privacy loads successfully", %{conn: conn} do
