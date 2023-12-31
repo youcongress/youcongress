3d9dd5a61a9c5f42e344e72ec371dcdded3c1645
Hector Perez <hecpeare@gmail.com>
2023-12-31 14:52:16 +0100

    Simplify TopHeader for mobile

--- lib/you_congress_web/components/top_header.ex
+++ lib/you_congress_web/components/top_header.ex
@@ -15,14 +15,15 @@ defmodule YouCongressWeb.TopHeaderComponent do
             YouCongress
           </a>
         </div>
+
         <div class="flex items-center gap-4 font-semibold leading-6 text-zinc-900">
           <%= if assigns[:votes_count] && assigns[:user_votes_count] do %>
-            <div>
+            <div class="hidden md:block">
               <%= @votes_count %> votes (<.link href={~p"/authors/#{@current_user.author_id}"}><%= @user_votes_count %> yours</.link>)
             </div>
           <% end %>
           <%= if @current_user do %>
-            <div class="text-[0.8125rem] leading-6 text-zinc-900">
+            <div class="hidden md:block text-[0.8125rem] leading-6 text-zinc-900">
               <.link href={~p"/authors/#{@current_user.author_id}"}>
                 <%= @current_user.email %>
               </.link>
