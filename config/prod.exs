1204303f34a3f725ea5a6ad19d9e04f6a5f65a04
Hector Perez <hecpeare@gmail.com>
2023-12-31 11:02:55 +0100

    Add check_origin to endpoint

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :you_congress, YouCongressWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  check_origin: ["https://youcongress.com", "https://staging.youcongress.com"]
 
 # Configures Swoosh API Client
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: YouCongress.Finch
