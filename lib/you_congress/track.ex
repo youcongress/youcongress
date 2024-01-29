34b7f32f5ffa0aefa5bef6f1d6ab4ede35614038
Hector Perez <hecpeare@gmail.com>
2024-01-29 20:05:02 +0100

    Make sure author is loaded when tracking events

--- lib/you_congress/track.ex
+++ lib/you_congress/track.ex
@@ -5,19 +5,21 @@ defmodule YouCongress.Track do
 
   @api_url "https://api.eu.amplitude.com/2/httpapi"
 
+  alias YouCongress.Authors
+
   def event(event_type, current_user) do
     api_key = Application.get_env(:you_congress, :amplitude_api_key)
 
     if api_key do
       spawn(fn ->
-        twitter_username = current_user.author.twitter_username
+        author = Authors.get_author!(current_user.author_id)
 
         body = %{
           "api_key" => api_key,
           "events" => [
             %{
               "event_type" => event_type,
-              "user_id" => amplitude_user_id(twitter_username, current_user)
+              "user_id" => amplitude_user_id(author.twitter_username, current_user)
             }
           ]
         }
