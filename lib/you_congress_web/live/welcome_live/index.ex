e38d9d93bb1da6a022345fb1957fb948348cfb16
Hector Perez <hecpeare@gmail.com>
2024-03-02 13:18:27 +0100

    Add onboarding info on /welcome

--- lib/you_congress_web/live/welcome_live/index.ex
+++ lib/you_congress_web/live/welcome_live/index.ex
@@ -1,13 +1,9 @@
 defmodule YouCongressWeb.WelcomeLive.Index do
   use YouCongressWeb, :live_view
 
-  alias YouCongress.Votings.Voting
-
   @impl true
   def mount(_params, session, socket) do
-    socket =
-      socket
-      |> assign_current_user(session["user_token"])
+    socket = assign_current_user(socket, session["user_token"])
 
     if connected?(socket) do
       %{assigns: %{current_user: current_user}} = socket
@@ -26,11 +22,6 @@ defmodule YouCongressWeb.WelcomeLive.Index do
     socket
     |> assign(:page_title, "Welcome")
     |> assign(:voting, nil)
-  end
-
-  @impl true
-  def handle_info({YouCongressWeb.VotingLive.NewFormComponent, {:put_flash, type, msg}}, socket) do
-    {:noreply, put_flash(socket, type, msg)}
   end
 
   @impl true
