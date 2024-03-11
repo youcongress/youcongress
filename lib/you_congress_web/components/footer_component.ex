a82e1b9bb12c749bfb6ff7cc5f3184ab303d92c8
Hector Perez <hecpeare@gmail.com>
2024-03-11 21:20:39 +0100

    Add GitHub, Telegram and email footer links

--- lib/you_congress_web/components/footer_component.ex
+++ lib/you_congress_web/components/footer_component.ex
@@ -8,9 +8,12 @@ defmodule YouCongressWeb.Components.FooterComponent do
 
   def footer(assigns) do
     ~H"""
-    <div class="text-center pb-4">
-      <.link href={~p"/terms"} class="text-sm">Terms</.link>
-      · <.link href={~p"/privacy-policy"} class="text-sm">Privacy</.link>
+    <div class="text-center pb-4 text-sm">
+      <.link href="https://github.com/youcongress/youcongress" target="_blank">GitHub</.link>
+      · <.link href="https://web.telegram.org/a/#-1002011576166" target="_blank">Telegram</.link>
+      · <.link href={~p"/terms"}>Terms</.link>
+      · <.link href={~p"/privacy-policy"}>Privacy</.link>
+      · <.link href="mailto:hi@youcongress.com">Contact</.link>
     </div>
     """
   end
