4ee0c08ba9ece5619dae36a81fcf7107af1b9d5e
Hector Perez <hecpeare@gmail.com>
2024-03-25 11:33:48 +0100

    Move amplitude requests to worker

--- lib/you_congress/track.ex
+++ lib/you_congress/track.ex
@@ -10,33 +10,38 @@ defmodule YouCongress.Track do
   def event(_, nil), do: nil
 
   def event(event_type, current_user) do
+    %{event_type: event_type, current_user_id: current_user.id, author_id: current_user.author_id}
+    |> YouCongress.Workers.TrackWorker.new()
+    |> Oban.insert()
+  end
+
+  def track_now(event_type, current_user_id, author_id) do
     api_key = Application.get_env(:you_congress, :amplitude_api_key)
 
     if api_key do
-      spawn(fn ->
-        author = Authors.get_author!(current_user.author_id)
+      author = Authors.get_author!(author_id)
 
-        body = %{
-          "api_key" => api_key,
-          "events" => [
-            %{
-              "event_type" => event_type,
-              "user_id" => amplitude_user_id(author.twitter_username, current_user)
-            }
-          ]
-        }
+      body = %{
+        "api_key" => api_key,
+        "events" => [
+          %{
+            "event_type" => event_type,
+            "user_id" => amplitude_user_id(author.twitter_username, current_user_id)
+          }
+        ]
+      }
 
-        headers = [{"Content-Type", "application/json"}, {"Accept", "*/*"}]
+      headers = [{"Content-Type", "application/json"}, {"Accept", "*/*"}]
 
-        Finch.build(:post, @api_url, headers, Jason.encode!(body))
-        |> Finch.request(YouCongress.Finch)
-      end)
+      :post
+      |> Finch.build(@api_url, headers, Jason.encode!(body))
+      |> Finch.request(YouCongress.Finch)
     end
   end
 
-  defp amplitude_user_id(nil, current_user), do: "user_id:" <> current_user.id
+  defp amplitude_user_id(nil, current_user_id), do: "user_id:#{current_user_id}"
 
-  defp amplitude_user_id(twitter_username, current_user) do
-    "twitter:" <> twitter_username <> "; user_id:#{current_user.id}"
+  defp amplitude_user_id(twitter_username, current_user_id) do
+    "twitter:" <> twitter_username <> "; user_id:#{current_user_id}"
   end
 end
