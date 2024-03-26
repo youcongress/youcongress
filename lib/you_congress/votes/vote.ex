165d8ee22e83373639b6d7c4f7f03b474ee5db97
Hector Perez <hecpeare@gmail.com>
2024-03-26 17:06:31 +0100

    Remove votes.source_id as it is now in opinions.source_id

--- lib/you_congress/votes/vote.ex
+++ lib/you_congress/votes/vote.ex
@@ -13,7 +13,6 @@ defmodule YouCongress.Votes.Vote do
   schema "votes" do
     field :direct, :boolean, default: true
     field :twin, :boolean, default: false
-    field :source_url, :binary
 
     belongs_to :author, Author
     belongs_to :voting, Voting
@@ -30,7 +29,6 @@ defmodule YouCongress.Votes.Vote do
           voting_id: integer(),
           answer_id: integer(),
           opinion_id: integer(),
-          source_url: binary(),
           inserted_at: NaiveDateTime.t(),
           updated_at: NaiveDateTime.t()
         }
@@ -43,7 +41,6 @@ defmodule YouCongress.Votes.Vote do
       :author_id,
       :voting_id,
       :answer_id,
-      :source_url,
       :opinion_id
     ])
     |> validate_required([:author_id, :voting_id, :answer_id])
