cf92fe8cda0a3630c39bc47d69574f976a4a6479
Hector Perez <hecpeare@gmail.com>
2024-04-05 23:18:31 +0200

    Display I am ... on real quotes

--- lib/you_congress_web/live/voting_live/vote_component/ai_quote_menu.ex
+++ lib/you_congress_web/live/voting_live/vote_component/ai_quote_menu.ex
@@ -7,12 +7,12 @@ defmodule YouCongressWeb.VotingLive.VoteComponent.AiQuoteMenu do
   """
   use Phoenix.Component
 
-  attr :vote_id, :string, required: true
+  attr :vote, :map, required: true
   attr :author_name, :string, required: true
 
   def render(assigns) do
     ~H"""
-    <div class="justify-end" phx-hook="AIQuote" id={"ai-menu-#{@vote_id}"}>
+    <div class="justify-end" phx-hook="AIQuote" id={"ai-menu-#{@vote.id}"}>
       <button
         type="button"
         class="inline-flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900"
@@ -24,7 +24,9 @@ defmodule YouCongressWeb.VotingLive.VoteComponent.AiQuoteMenu do
       </button>
       <div class="hidden absolute right-0 lg:right-0 z-10 lg:mt-5 flex lg:w-screen max-w-full lg:max-w-min lg:-translate-x-1/2 lg:px-1/2">
         <div class="w-56 shrink rounded-xl bg-white p-4 text-sm font-semibold leading-6 text-gray-900 shadow-lg ring-1 ring-gray-900/5">
-          <a href="/faq#ai-profiles" class="block p-2 hover:text-indigo-600">About AI profiles</a>
+          <%= if @vote.twin do %>
+            <a href="/faq#ai-profiles" class="block p-2 hover:text-indigo-600">About AI profiles</a>
+          <% end %>
           <a href="/faq#change-ai-profile" class="block p-2 hover:text-indigo-600">
             I am <%= @author_name %>
           </a>
