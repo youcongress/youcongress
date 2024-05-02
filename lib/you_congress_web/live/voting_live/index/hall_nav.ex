272346453b8549fe79da4d767e279bd733d189de
Hector Perez <hecpeare@gmail.com>
2024-05-02 13:32:05 +0200

    Add more halls in mobile

--- lib/you_congress_web/live/voting_live/index/hall_nav.ex
+++ lib/you_congress_web/live/voting_live/index/hall_nav.ex
@@ -1,5 +1,6 @@
 defmodule YouCongressWeb.VotingLive.Index.HallNav do
   use Phoenix.Component
+  use Phoenix.VerifiedRoutes, endpoint: YouCongressWeb.Endpoint, router: YouCongressWeb.Router
 
   alias YouCongressWeb.VotingLive.Index.HallNav
   alias YouCongress.Tools.StringUtils
@@ -9,91 +10,108 @@ defmodule YouCongressWeb.VotingLive.Index.HallNav do
   def render(assigns) do
     ~H"""
     <div class="border-b border-gray-200">
-      <div class="hidden md:block">
+      <div class="pb-2">
         <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-          <HallNav.tab url_hall_name={@hall_name} hall_name="ai" hall_link="/" hall_title="AI" />
-          <%= if @hall_name not in ["ai", "climate", "space", "spain", "eu", "us", "law", "programming", "all"] do %>
+          <div class="pt-1 space-x-8">
+            <HallNav.tab url_hall_name={@hall_name} hall_name="ai" hall_link="/" hall_title="AI" />
             <HallNav.tab
               url_hall_name={@hall_name}
-              hall_name={@hall_name}
-              hall_link="/halls/#{@hall_name}"
-              hall_title={StringUtils.titleize(@hall_name)}
+              hall_name="spain"
+              hall_link={~p"/halls/spain"}
+              hall_title="Spain"
             />
-          <% end %>
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="eu"
+              hall_link={~p"/halls/eu"}
+              hall_title="EU"
+            />
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="us"
+              hall_link={~p"/halls/us"}
+              hall_title="US"
+            />
+          </div>
+          <div class="hidden md:block pt-1 space-x-8">
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="climate"
+              hall_link={~p"/halls/climate"}
+              hall_title="Climate"
+            />
+            <%= if @hall_name not in ["programming", "ai", "climate", "space", "spain", "eu", "us", "law", "programming", "all"] do %>
+              <HallNav.tab
+                url_hall_name={@hall_name}
+                hall_name={@hall_name}
+                hall_link={~p"/halls/#{@hall_name}"}
+                hall_title={StringUtils.titleize(@hall_name)}
+              />
+            <% else %>
+              <HallNav.tab
+                url_hall_name={@hall_name}
+                hall_name="programming"
+                hall_link={~p"/halls/programming"}
+                hall_title="Programming"
+              />
+            <% end %>
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="law"
+              hall_link={~p"/halls/law"}
+              hall_title="Law"
+            />
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="space"
+              hall_link={~p"/halls/space"}
+              hall_title="Space"
+            />
+          </div>
+          <div class="pt-1 space-x-8">
+            <HallNav.tab
+              url_hall_name={@hall_name}
+              hall_name="all"
+              hall_link={~p"/halls/all"}
+              hall_title="All"
+            />
+          </div>
+        </nav>
+      </div>
+      <div class="-mb-px flex space-x-8 md:hidden">
+        <%= if @hall_name not in ["programming", "ai", "climate", "space", "spain", "eu", "us", "law", "programming", "all"] do %>
+          <HallNav.tab
+            url_hall_name={@hall_name}
+            hall_name={@hall_name}
+            hall_link={~p"/halls/#{@hall_name}"}
+            hall_title={StringUtils.titleize(@hall_name)}
+          />
+        <% else %>
           <HallNav.tab
             url_hall_name={@hall_name}
             hall_name="climate"
-            hall_link="/halls/climate"
+            hall_link={~p"/halls/climate"}
             hall_title="Climate"
-          />
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="space"
-            hall_link="/halls/space"
-            hall_title="Space"
-          />
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="spain"
-            hall_link="/halls/spain"
-            hall_title="Spain"
-          />
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="eu"
-            hall_link="/halls/eu"
-            hall_title="EU"
-          />
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="us"
-            hall_link="/halls/us"
-            hall_title="US"
-          />
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="law"
-            hall_link="/halls/law"
-            hall_title="Law"
           />
           <HallNav.tab
             url_hall_name={@hall_name}
             hall_name="programming"
-            hall_link="/halls/programming"
+            hall_link={~p"/halls/programming"}
             hall_title="Programming"
           />
           <HallNav.tab
             url_hall_name={@hall_name}
-            hall_name="all"
-            hall_link="/halls/all"
-            hall_title="All"
-          />
-        </nav>
-      </div>
-      <div class="md:hidden">
-        <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-          <HallNav.tab url_hall_name={@hall_name} hall_name="ai" hall_link="/" hall_title="AI" />
-          <%= if @hall_name not in ["ai", "climate", "all"] do %>
-            <HallNav.tab
-              url_hall_name={@hall_name}
-              hall_name={@hall_name}
-              hall_link="/halls/#{@hall_name}"
-              hall_title={StringUtils.titleize(@hall_name)}
-            />
-          <% end %>
-          <HallNav.tab
-            url_hall_name={@hall_name}
-            hall_name="climate"
-            hall_link="/halls/climate"
-            hall_title="Climate"
+            hall_name="law"
+            hall_link={~p"/halls/law"}
+            hall_title="Law"
           />
           <HallNav.tab
             url_hall_name={@hall_name}
-            hall_name="all"
-            hall_link="/halls/all"
-            hall_title="All"
+            hall_name="space"
+            hall_link={~p"/halls/space"}
+            hall_title="Space"
           />
-        </nav>
+        <% end %>
       </div>
     </div>
     """
@@ -111,9 +129,9 @@ defmodule YouCongressWeb.VotingLive.Index.HallNav do
       href={@hall_link}
       class={[
         @url_hall_name == @hall_name &&
-          "border-indigo-500 text-indigo-600 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium",
+          "border-indigo-500 text-indigo-600 whitespace-nowrap border-b-2 py-2 px-1 text-sm font-medium",
         @url_hall_name != @hall_name &&
-          "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap border-b-2 py-4 px-1 text-sm font-medium"
+          "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 whitespace-nowrap border-b-2 py-2 px-1 text-sm font-medium"
       ]}
       aria-current="page"
     >
