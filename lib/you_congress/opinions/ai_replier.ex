b505e57cd7d7f774adf5ff2110f536b408a657ce
Hector Perez <hecpeare@gmail.com>
2024-07-06 19:13:07 +0200

    Minor refactor

--- lib/you_congress/opinions/ai_replier.ex
+++ lib/you_congress/opinions/ai_replier.ex
@@ -5,45 +5,46 @@ defmodule YouCongress.Opinions.AIReplier do
 
   alias YouCongress.Opinions.Opinion
 
-  def reply(%{twin: true}), do: do_nothing()
-  def reply(%{ancestry: nil}), do: do_nothing()
+  def maybe_reply(%{twin: true}), do: do_nothing()
+  def maybe_reply(%{ancestry: nil}), do: do_nothing()
 
-  def reply(opinion) do
+  def maybe_reply(opinion) do
     if Opinion.parent(opinion).twin do
-      voting = YouCongress.Votings.get_voting!(opinion.voting_id)
-      ancestor_and_self_ids = YouCongress.Opinions.Opinion.path_ids(opinion)
-
-      ancestors_and_self =
-        YouCongress.Opinions.list_opinions(
-          ids: ancestor_and_self_ids,
-          preload: [:author],
-          order_by: [desc: :id]
-        )
-
-      case YouCongress.Opinions.AIComment.generate_comment(
-             voting.title,
-             ancestors_and_self,
-             :"gpt-4o"
-           ) do
-        {:ok, %{reply: content, author_id: author_id}} ->
-          IO.inspect(content)
-
-          YouCongress.Opinions.create_opinion(%{
-            "content" => content,
-            "author_id" => author_id,
-            "voting_id" => opinion.voting_id,
-            "ancestry" => set_ancestry(opinion),
-            "twin" => true
-          })
-          |> IO.inspect()
-
-          :ok
-
-        {:error, _} ->
-          do_nothing()
-      end
+      reply(opinion)
     else
       do_nothing()
+    end
+  end
+
+  defp reply(opinion) do
+    voting = YouCongress.Votings.get_voting!(opinion.voting_id)
+    ancestor_and_self_ids = YouCongress.Opinions.Opinion.path_ids(opinion)
+
+    ancestors_and_self =
+      YouCongress.Opinions.list_opinions(
+        ids: ancestor_and_self_ids,
+        preload: [:author],
+        order_by: [desc: :id]
+      )
+
+    case YouCongress.Opinions.AIComment.generate_comment(
+           voting.title,
+           ancestors_and_self,
+           :"gpt-4o"
+         ) do
+      {:ok, %{reply: content, author_id: author_id}} ->
+        YouCongress.Opinions.create_opinion(%{
+          "content" => content,
+          "author_id" => author_id,
+          "voting_id" => opinion.voting_id,
+          "ancestry" => set_ancestry(opinion),
+          "twin" => true
+        })
+
+        :ok
+
+      {:error, _} ->
+        do_nothing()
     end
   end
 
--- lib/you_congress_web/live/opinion_live/show.ex
+++ lib/you_congress_web/live/opinion_live/show.ex
@@ -70,7 +70,7 @@ defmodule YouCongressWeb.OpinionLive.Show do
         Track.event("New Opinion", current_user)
 
         # We do this synchronous as we want the reply be ready when we redirect
-        YouCongress.Opinions.AIReplier.reply(opinion)
+        YouCongress.Opinions.AIReplier.maybe_reply(opinion)
 
         {:noreply,
          socket
