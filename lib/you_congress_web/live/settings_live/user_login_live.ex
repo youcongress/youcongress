965cf9114bac1994934565a8e9ef0f7135ab0b72
Hector Perez <hecpeare@gmail.com>
2024-08-28 13:44:02 +0200

    Remove forgot your password link for now

--- lib/you_congress_web/live/settings_live/user_login_live.ex
+++ lib/you_congress_web/live/settings_live/user_login_live.ex
@@ -23,9 +23,6 @@ defmodule YouCongressWeb.UserLoginLive do
 
         <:actions>
           <.input field={@form[:remember_me]} type="checkbox" label="Keep me logged in" />
-          <.link href={~p"/reset_password"} class="text-sm font-semibold">
-            Forgot your password?
-          </.link>
         </:actions>
         <:actions>
           <.button phx-disable-with="Signing in..." class="w-full">
