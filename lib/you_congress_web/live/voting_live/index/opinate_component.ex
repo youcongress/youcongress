a7b366bc3ceffc7f0e941e3c356bf2a080874778
Hector Perez <hecpeare@gmail.com>
2024-10-29 20:52:21 +0100

    Add read comments link when logged too

--- lib/you_congress_web/live/voting_live/index/opinate_component.ex
+++ lib/you_congress_web/live/voting_live/index/opinate_component.ex
@@ -45,14 +45,9 @@ defmodule YouCongressWeb.VotingLive.Index.OpinateComponent do
             <textarea
               name="opinion[content]"
               rows="4"
-              placeholder={
-                if @current_user,
-                  do: "Add your comment...",
-                  else: "Log in to comment..."
-              }
+              placeholder="Add your comment..."
               class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
               phx-debounce="300"
-              disabled={is_nil(@current_user)}
             ><%= @form[:content].value %></textarea>
             <%= for error <- Keyword.get_values(@form.errors, :content) do %>
               <div class="mt-1 text-sm text-red-600"><%= translate_error(error) %></div>
@@ -63,7 +58,6 @@ defmodule YouCongressWeb.VotingLive.Index.OpinateComponent do
               type="submit"
               class="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:opacity-50 disabled:cursor-not-allowed"
               phx-disable-with="Saving..."
-              disabled={is_nil(@current_user)}
             >
               <%= if @form.data.id, do: "Update", else: "Publish" %> Arguments
             </button>
@@ -86,6 +80,10 @@ defmodule YouCongressWeb.VotingLive.Index.OpinateComponent do
                 >
                   Cancel
                 </.link>
+              <% else %>
+                <span class="pl-2">
+                  or <.link href={~p"/p/#{@voting.slug}"} class="underline">read comments</.link>
+                </span>
               <% end %>
             <% end %>
           </div>
