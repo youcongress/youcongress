9a3daef5a71ad6187ea6698eae5102fe6211a1c4
Hector Perez <hecpeare@gmail.com>
2024-12-20 22:34:51 +0100

    Action summit halls (#139)

--- lib/you_congress/digital_twins/open_ai_model.ex
+++ lib/you_congress/digital_twins/open_ai_model.ex
@@ -6,10 +6,8 @@ defmodule YouCongress.DigitalTwins.OpenAIModel do
   @type t :: :"gpt-3.5-turbo-0125" | :"gpt-4" | :"gpt-4-turbo-2024-04-09" | :"gpt-4o"
 
   @token_cost %{
-    :"gpt-4o" => %{completion_tokens: 0.015, prompt_tokens: 0.005},
-    :"gpt-4-turbo-2024-04-09" => %{completion_tokens: 0.03, prompt_tokens: 0.01},
-    :"gpt-4" => %{completion_tokens: 0.06, prompt_tokens: 0.03},
-    :"gpt-3.5-turbo-0125" => %{completion_tokens: 0.002, prompt_tokens: 0.0015}
+    :"gpt-4o" => %{completion_tokens: 0.01, prompt_tokens: 0.0025},
+    :"gpt-4o-mini" => %{completion_tokens: 0.0006, prompt_tokens: 0.00015},
   }
 
   @spec get_content(map) :: [binary]
