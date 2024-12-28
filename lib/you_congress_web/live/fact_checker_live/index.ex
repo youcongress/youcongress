edd56bed8292f5218482ca1a242ebb78a6896405
Hector Perez <hecpeare@gmail.com>
2024-12-28 23:51:27 +0100

    Max text length

--- lib/you_congress_web/live/fact_checker_live/index.ex
+++ lib/you_congress_web/live/fact_checker_live/index.ex
@@ -1,6 +1,8 @@
 defmodule YouCongressWeb.FactCheckerLive.Index do
   use YouCongressWeb, :live_view
 
+  @max_text_length 3000
+
   import Logger
 
   alias YouCongress.FactChecker
@@ -8,11 +10,17 @@ defmodule YouCongressWeb.FactCheckerLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, current_text: "", loading: false)}
+    {:ok, assign(socket, current_text: "")}
   end
 
   @impl true
+  def handle_event("fact_check", %{"text" => ""}, socket) do
+    {:noreply, socket}
+  end
+
   def handle_event("fact_check", %{"text" => text}, socket) when is_binary(text) do
+    text = maybe_truncate_text(text)
+
     case FactChecker.classify_text(text) do
       {:ok, analyzed} ->
         {:noreply,
@@ -28,4 +36,12 @@ defmodule YouCongressWeb.FactCheckerLive.Index do
   end
 
   def handle_event("fact_check", _, socket), do: {:noreply, socket}
+
+  defp maybe_truncate_text(text) do
+    text = String.trim(text)
+    initial_length = String.length(text)
+    text = String.slice(text, 0, @max_text_length)
+    final_length = String.length(text)
+    text = if final_length < initial_length, do: "#{text}...", else: text
+  end
 end
