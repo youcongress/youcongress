37974acdae195adf32caa21ca7f35aa2aa547aef
Hector Perez <hecpeare@gmail.com>
2025-04-19 00:44:19 +0200

    X login button in the home page

--- lib/you_congress_web/live/settings_live/user_login_live.ex
+++ lib/you_congress_web/live/settings_live/user_login_live.ex
@@ -11,23 +11,21 @@ defmodule YouCongressWeb.UserLoginLive do
         </:subtitle>
       </.header>
 
-      <div class="pt-4">
-        <div class="text-center">
-          <.link
-            href="/x_log_in"
-            class="inline-flex items-center justify-between bg-black text-white font-bold py-2 px-4 rounded-full hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 transition-colors duration-300"
+      <div class="text-center pt-4">
+        <.link
+          href="/x_log_in"
+          class="inline-flex items-center justify-between bg-black text-white font-bold py-2 px-4 rounded-full hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-600 focus:ring-opacity-50 transition-colors duration-300"
+        >
+          Sign in with
+          <svg
+            class="w-5 h-5 ml-2"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#ffffff"
           >
-            Sign in with
-            <svg
-              class="w-5 h-5 ml-2"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="#ffffff"
-            >
-              <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
-            </svg>
-          </.link>
-        </div>
+            <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+          </svg>
+        </.link>
       </div>
       <div class="pt-4 text-center">or</div>
 
