0e97f4439cad7418c1045da9c11d40df22293ca6
Hector Perez <hecpeare@gmail.com>
2025-04-19 02:23:18 +0200

    No root layout in sim

--- lib/you_congress_web/controllers/sim_controller.ex
+++ lib/you_congress_web/controllers/sim_controller.ex
@@ -1,7 +1,10 @@
 defmodule YouCongressWeb.SimController do
   use YouCongressWeb, :controller
+  import Phoenix.Controller
 
   def index(conn, _params) do
-    render(conn, :index, layout: false)
+    conn
+    |> put_root_layout(false)
+    |> render(:index, layout: false)
   end
 end
