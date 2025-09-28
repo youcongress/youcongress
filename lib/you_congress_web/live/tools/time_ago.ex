5c713a9b498d8783af3492df2f7ed96ff38fda86
Hector Perez <hecpeare@gmail.com>
2025-09-28 19:45:16 +0200

    Downcase added x time ago

--- lib/you_congress_web/live/tools/time_ago.ex
+++ lib/you_congress_web/live/tools/time_ago.ex
@@ -5,14 +5,25 @@ defmodule YouCongressWeb.Tools.TimeAgo do
 
   def short_time(datetime)
       when is_struct(datetime, DateTime) or is_struct(datetime, NaiveDateTime) do
-    datetime =
-      case datetime do
-        %NaiveDateTime{} -> DateTime.from_naive!(datetime, "Etc/UTC")
-        %DateTime{} -> datetime
-      end
+    datetime
+    |> normalize_datetime()
+    |> seconds_ago()
+    |> format_short_diff()
+  end
 
-    diff = DateTime.diff(DateTime.utc_now(), datetime, :second)
+  def short_time(_), do: ""
 
+  defp normalize_datetime(%DateTime{} = datetime), do: datetime
+
+  defp normalize_datetime(%NaiveDateTime{} = datetime) do
+    DateTime.from_naive!(datetime, "Etc/UTC")
+  end
+
+  defp seconds_ago(datetime) do
+    DateTime.diff(DateTime.utc_now(), datetime, :second)
+  end
+
+  defp format_short_diff(diff) do
     cond do
       diff < 60 -> "#{diff}s ago"
       diff < 3600 -> "#{div(diff, 60)}min ago"
@@ -22,6 +33,4 @@ defmodule YouCongressWeb.Tools.TimeAgo do
       true -> "#{div(diff, 31_536_000)}y ago"
     end
   end
-
-  def short_time(_), do: ""
 end
