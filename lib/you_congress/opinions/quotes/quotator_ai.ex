95fa003fd163a6ef0f21d1527ea508526b177949
Hector Perez <hecpeare@gmail.com>
2025-10-12 09:19:15 +0200

    Try gpt-5-mini

--- lib/you_congress/opinions/quotes/quotator_ai.ex
+++ lib/you_congress/opinions/quotes/quotator_ai.ex
@@ -8,7 +8,7 @@ defmodule YouCongress.Opinions.Quotes.QuotatorAI do
   alias YouCongress.DigitalTwins.OpenAIModel
   alias YouCongress.Opinions.Quotes.Quotator
 
-  @model :"gpt-5"
+  @model :"gpt-5-mini"
   @timeout_in_min 120
 
   def number_of_quotes, do: Quotator.number_of_quotes()
