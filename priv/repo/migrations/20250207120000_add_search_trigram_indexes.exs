f5fd411e646980b71cc858b5bcc174700eabd95c
Hector Perez <hecpeare@gmail.com>
2025-12-08 15:45:04 +0100

    Fix migration

--- priv/repo/migrations/20250207120000_add_search_trigram_indexes.exs
+++ priv/repo/migrations/20250207120000_add_search_trigram_indexes.exs
@@ -4,35 +4,30 @@ defmodule YouCongress.Repo.Migrations.AddSearchTrigramIndexes do
   def up do
     execute("CREATE EXTENSION IF NOT EXISTS pg_trgm")
 
-    create index(:votings, [:title],
-             name: :votings_title_trgm_index,
-             using: :gin,
-             opclass: :gin_trgm_ops
-           )
+    execute("""
+    CREATE INDEX IF NOT EXISTS votings_title_trgm_index
+    ON votings USING GIN (title gin_trgm_ops)
+    """)
 
-    create index(:authors, [:name],
-             name: :authors_name_trgm_index,
-             using: :gin,
-             opclass: :gin_trgm_ops
-           )
+    execute("""
+    CREATE INDEX IF NOT EXISTS authors_name_trgm_index
+    ON authors USING GIN (name gin_trgm_ops)
+    """)
 
-    create index(:authors, [:twitter_username],
-             name: :authors_twitter_username_trgm_index,
-             using: :gin,
-             opclass: :gin_trgm_ops
-           )
+    execute("""
+    CREATE INDEX IF NOT EXISTS authors_twitter_username_trgm_index
+    ON authors USING GIN (twitter_username gin_trgm_ops)
+    """)
 
-    create index(:halls, [:name],
-             name: :halls_name_trgm_index,
-             using: :gin,
-             opclass: :gin_trgm_ops
-           )
+    execute("""
+    CREATE INDEX IF NOT EXISTS halls_name_trgm_index
+    ON halls USING GIN (name gin_trgm_ops)
+    """)
 
-    create index(:opinions, [:content],
-             name: :opinions_content_trgm_index,
-             using: :gin,
-             opclass: :gin_trgm_ops
-           )
+    execute("""
+    CREATE INDEX IF NOT EXISTS opinions_content_trgm_index
+    ON opinions USING GIN (content gin_trgm_ops)
+    """)
   end
 
   def down do
