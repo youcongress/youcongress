06e41e9237e3572e63be16930e3f01bfff504217
Hector Perez <hecpeare@gmail.com>
2026-01-04 13:05:02 +0100

    Search MCP tool takes opinions from YouCongress

--- lib/you_congress_web/mcp_server/search.ex
+++ lib/you_congress_web/mcp_server/search.ex
@@ -1,15 +1,30 @@
 defmodule YouCongressWeb.MCPServer.Search do
-  @moduledoc "Search quotes, policy proposals, claims, authors, etc."
+  @moduledoc "Search quotes and authors on YouCongress."
 
   use Anubis.Server.Component, type: :tool
 
   alias Anubis.Server.Response
+  alias YouCongress.Opinions
 
   schema do
     field :query, :string, required: true
   end
 
   def execute(%{query: query}, frame) do
-    {:reply, Response.json(Response.tool(), "Hello #{query}"), frame}
+    opinions =
+      [search: query, preload: [:author]]
+      |> Opinions.list_opinions()
+      |> Enum.map(fn opinion ->
+        %{
+          quote: opinion.content,
+          author: opinion.author.name
+        }
+      end)
+
+    data = %{
+      quotes: opinions
+    }
+
+    {:reply, Response.json(Response.tool(), data), frame}
   end
 end
