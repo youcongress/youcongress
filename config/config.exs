bb0aae83533766d601d39b6dbf333bd3f1f4811a
Hector Perez <hecpeare@gmail.com>
2026-08-21 23:04:02 +0200

    Disable API-based fresh agents

--- config/config.exs
+++ config/config.exs
@@ -73,7 +73,7 @@ oban_plugins =
     [
       {Oban.Plugins.Cron,
        crontab: [
-         {"0 5,17 * * *", YouCongress.Workers.FreshQuoteDiscoveryWorker}
+#         {"0 5,17 * * *", YouCongress.Workers.FreshQuoteDiscoveryWorker}
        ]}
       | oban_plugins
     ]
