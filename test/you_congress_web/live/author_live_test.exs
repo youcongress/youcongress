f1ceba4cdbc6165fef495526eb29caa5ac542f1b
Hector Perez <hecpeare@gmail.com>
2026-09-24 17:32:51 +0000

    Increase related author limit to nine

--- test/you_congress_web/live/author_live_test.exs
+++ test/you_congress_web/live/author_live_test.exs
@@ -104,7 +104,7 @@ defmodule YouCongressWeb.AuthorLiveTest do
       refute has_element?(show_live, "a", "Wikipedia")
     end
 
-    test "suggests up to six other authors with sourced quotes in the same halls", %{
+    test "suggests up to nine other authors with sourced quotes in the same halls", %{
       conn: conn,
       author: author
     } do
@@ -131,11 +131,13 @@ defmodule YouCongressWeb.AuthorLiveTest do
       })
 
       related_authors =
-        Enum.map(1..7, fn number ->
+        Enum.map(1..10, fn number ->
+          number_label = number |> Integer.to_string() |> String.pad_leading(2, "0")
+
           related_author =
             author_fixture(%{
-              name: "Related Author #{number}",
-              twitter_username: "related_author_#{number}",
+              name: "Related Author #{number_label}",
+              twitter_username: "related_author_#{number_label}",
               bio: "Related bio #{number}",
               profile_image_url: "https://example.com/related-#{number}.jpg"
             })
@@ -155,9 +157,9 @@ defmodule YouCongressWeb.AuthorLiveTest do
       {:ok, show_live, html} = live(conn, ~p"/x/#{author.twitter_username}")
 
       assert html =~ "Other authors to follow"
-      assert length(Regex.scan(~r/data-testid="other-author"/, html)) == 6
+      assert length(Regex.scan(~r/data-testid="other-author"/, html)) == 9
 
-      Enum.each(Enum.take(related_authors, 6), fn related_author ->
+      Enum.each(Enum.take(related_authors, 9), fn related_author ->
         assert has_element?(
                  show_live,
                  "#other-authors a[href='/x/#{related_author.twitter_username}']",
